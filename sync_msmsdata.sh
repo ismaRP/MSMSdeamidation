59631c40d51a55af98af4894fd3f725117c17646
ismaRP <isma.kosspub@gmail.com>
2021-01-13 10:41:48 +0100

    script to synchronize mq msms data with drive

---
+++ sync_msmsdata.sh
@@ -0,0 +1,6 @@
+# Synchornize MSMS data
+
+if [ $1 == 'push' ]; then
+  rclone sync MSMSdatasets palaeome:MSMSdatasets -P
+elif [ $1 == 'pull' ]; then
+  rclone sync palaeome:MSMSdatasets MSMSdatasets -P
